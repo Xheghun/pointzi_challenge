03d9b09d489d5c2ba5b23013e0a5135a6eafbcba
Xheghun <ysenruf@gmail.com>
2021-05-25 05:52:27 +0400

    custom dialog implementation

--- demo_library/src/main/java/com/xheghun/demo_library/fab/PumpFAB.kt
+++ demo_library/src/main/java/com/xheghun/demo_library/fab/PumpFAB.kt
@@ -1,4 +1,5 @@
 package com.xheghun.demo_library.fab
+
 import android.app.Activity
 import android.content.Context
 import android.view.View
@@ -7,9 +8,10 @@ import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.constraintlayout.widget.ConstraintSet
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.xheghun.demo_library.R
+import com.xheghun.demo_library.fab.presentation.CustomDialog
 
 class PumpFAB(act: Activity) {
-private var  activity: Activity = act
+    private var activity: Activity = act
 
     fun pumpFAB(context: Context, layoutId: Int) {
         val fab = FloatingActionButton(context)
@@ -17,23 +19,22 @@ private var  activity: Activity = act
         val layout = activity.findViewById<ViewGroup>(layoutId)
         fab.setImageResource(R.drawable.ic_add)
         layout.addView(fab)
-        if(layout is ConstraintLayout) {
+        alignFAB(layout, fab)
+
+
+        fab.setOnClickListener {
+            CustomDialog().showDialog(context, activity);
+        }
+    }
+
+    private fun alignFAB(layout: ViewGroup, fab: View) {
+        if (layout is ConstraintLayout) {
             val set = ConstraintSet()
             set.clone(layout)
             set.connect(fab.id, ConstraintSet.RIGHT, ConstraintSet.PARENT_ID, ConstraintSet.RIGHT)
             set.connect(fab.id, ConstraintSet.BOTTOM, ConstraintSet.PARENT_ID, ConstraintSet.BOTTOM)
             set.setMargin(fab.id, ConstraintSet.BOTTOM, 15)
             set.setMargin(fab.id, ConstraintSet.RIGHT, 15)
-            set.applyTo(layout);
-        }
-
-    }
-
-   private fun alignFAB(layout: ViewGroup, view: View) {
-        if(layout is ConstraintLayout) {
-            val set = ConstraintSet()
-            set.clone(layout)
-            set.connect(view.id, ConstraintSet.BOTTOM, layout.id, ConstraintSet.RIGHT)
             set.applyTo(layout);
         }
     }
--- demo_library/src/main/java/com/xheghun/demo_library/fab/presentation/CustomDialog.kt
+++ demo_library/src/main/java/com/xheghun/demo_library/fab/presentation/CustomDialog.kt
@@ -1,4 +1,21 @@
 package com.xheghun.demo_library.fab.presentation
 
+import android.app.Activity
+import android.app.AlertDialog
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import com.xheghun.demo_library.R
+
 class CustomDialog {
+   fun showDialog(context: Context, activity: Activity) {
+        val dialogBuilder: AlertDialog.Builder = AlertDialog.Builder(context)
+
+        val parent: ViewGroup = activity.window.decorView.rootView as (ViewGroup)
+
+        val dialogLayout = LayoutInflater.from(context).inflate(R.layout.dialog_layout, parent,false);
+        dialogBuilder.setView(dialogLayout)
+        val alertDialog: AlertDialog = dialogBuilder.create()
+        alertDialog.show()
+    }
 }
