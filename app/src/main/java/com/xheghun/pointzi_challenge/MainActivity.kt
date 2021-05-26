879497921dfb0eccc8b277cc7b8fee7a2265ede4
Xheghun <ysenruf@gmail.com>
2021-05-26 06:51:41 +0400

    replace constructor overloading with builder

--- app/src/main/java/com/xheghun/pointzi_challenge/MainActivity.kt
+++ app/src/main/java/com/xheghun/pointzi_challenge/MainActivity.kt
@@ -2,13 +2,22 @@ package com.xheghun.pointzi_challenge
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.xheghun.demo_library.fab.FabLibrary
+import com.xheghun.pointzi_challenge.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        FabLibrary(this,R.id.main_layout).init()
+//        FabLibrary(this,R.id.main_layout).init()
+        val builder = FabLibrary.Builder()
+//        builder.setRoot(binding.root as ViewGroup).build()
+        builder.setActivityParams(this, R.id.main_layout).build()
     }
 }
--- demo_library/src/main/java/com/xheghun/demo_library/fab/FabLibrary.kt
+++ demo_library/src/main/java/com/xheghun/demo_library/fab/FabLibrary.kt
@@ -1,6 +1,7 @@
 package com.xheghun.demo_library.fab
 
 import android.app.Activity
+import android.util.Log
 import android.view.View
 import android.view.ViewGroup
 import android.widget.RelativeLayout
@@ -11,42 +12,53 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.xheghun.demo_library.R
 import com.xheghun.demo_library.fab.presentation.CustomDialog
 
-class FabLibrary(act: Activity,  mLayoutId: Int) {
-    private val activity: Activity = act
-    private val layoutId = mLayoutId
+class FabLibrary(private val rootLayout: ViewGroup) {
 
-    private val fab = FloatingActionButton(activity)
-    private val layout = activity.findViewById<ViewGroup>(layoutId)
+    private var fab: FloatingActionButton = FloatingActionButton(rootLayout.context)
 
-    fun init() {
+    class Builder {
+
+        fun setRoot(root: ViewGroup): FabLibrary {
+            return FabLibrary(root)
+        }
+
+        fun setActivityParams(activity: Activity, layoutId: Int): FabLibrary {
+            val root = activity.findViewById<ViewGroup>(layoutId)
+            return FabLibrary(root)
+        }
+
+    }
+
+    fun build() {
         fab.id = View.generateViewId()
         fab.setImageResource(R.drawable.ic_add)
 
+        rootLayout.addView(fab)
 
-        layout.addView(fab)
         alignFAB()
         setFabMargin()
         fab.setOnClickListener {
-            CustomDialog(activity).show()
+            CustomDialog(rootLayout.context).show()
         }
     }
 
     private fun setFabMargin() {
         val margin = 20
         val layoutParam = fab.layoutParams as ViewGroup.MarginLayoutParams
-        layoutParam.setMargins(margin, margin,margin,margin)
+        layoutParam.setMargins(margin, margin, margin, margin)
         fab.layoutParams = layoutParam
     }
 
     private fun alignFAB() {
-        if (layout is ConstraintLayout) {
+        if (rootLayout is ConstraintLayout) {
+            val rootView = rootLayout as ConstraintLayout
             val set = ConstraintSet()
-            set.clone(layout)
+            set.clone(rootView)
             set.connect(fab.id, ConstraintSet.RIGHT, ConstraintSet.PARENT_ID, ConstraintSet.RIGHT)
             set.connect(fab.id, ConstraintSet.BOTTOM, ConstraintSet.PARENT_ID, ConstraintSet.BOTTOM)
-            set.applyTo(layout)
-        } else if (layout is RelativeLayout) {
-           val layoutParams =  fab.layoutParams as (LayoutParams)
+            set.applyTo(rootView)
+        } else if (rootLayout is RelativeLayout) {
+            val layoutParams = fab.layoutParams as (LayoutParams)
             layoutParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM)
             layoutParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT)
             fab.layoutParams = layoutParams
